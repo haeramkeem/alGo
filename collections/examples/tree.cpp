0b53f9d9ad3a285bd0045cff2e7364e3b21fca35
saltwalks <ewqdsacxz2345@gmail.com>
2021-09-30 00:10:06 +0900

    add order traversal of tree

--- collections/examples/tree.cpp
+++ collections/examples/tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
@@ -48,6 +49,51 @@ struct Tree {
         }
         return true;
     }
+
+    /**
+     * Preorder traversal is visiting current node first, and visit first, second node.
+     */
+    static void preOrder(Node* node) {
+        cout << node->strData << ' '; // (1) process current node
+        if(node->first != NULL) { preOrder(node->first); } // (2) process first node
+        if(node->second != NULL) { preOrder(node->second); } // (3) process second node
+    }
+
+    /**
+     * Inorder traversal is visiting first node first, and visit current, second node.
+     */
+    static void inOrder(Node* node) {
+        if(node->first != NULL) { inOrder(node->first); } // (1) process first node
+        cout << node->strData << ' '; // (2) process current node
+        if(node->second != NULL) { inOrder(node->second); } // (3) process second node
+    }
+
+    /**
+     * Postorder traversal is visiting first node first, and visit current, second node.
+     */
+    static void postOrder(Node* node) {
+        if(node->first != NULL) { postOrder(node->first); } // (1) process first node
+        if(node->second != NULL) { postOrder(node->second); } // (2) process second node
+        cout << node->strData << ' '; // (3) process current node
+    }
+
+    /**
+     * Postorder traversal is visiting by up-down, left-right order.
+     *     BFS(Breadth-First Search)is using Postorder traversal.
+     */
+    static void levelOrder(Node* start) {
+        if(!start) { return; }
+        queue<Node*> qQueue;
+        qQueue.push(start);
+        while(!qQueue.empty()) {
+            Node* h = qQueue.front();
+            qQueue.pop();
+            cout << h->strData << ' ';
+            if(h->first != NULL) { qQueue.push(h->first); }
+            if(h->second != NULL) { qQueue.push(h->second); }
+        }
+        cout << endl;
+    }
 };
 
 int main() {
@@ -56,5 +102,13 @@ int main() {
     tree.add("hello", "hola");
     tree.add("aloha", "gimmozzi");
     tree.add("aloha", "pizza");
+
+    Tree::preOrder(tree.root); // result is "hello aloha gimmozzi pizza hola"
+    cout << endl;
+    Tree::inOrder(tree.root); // result is "gimmozzi aloha pizza hello hola"
+    cout << endl;
+    Tree::postOrder(tree.root); // result is "gimmozzi pizza aloha hola hello"
+    cout << endl;
+    Tree::levelOrder(tree.root); // result is "hello aloha hola gimmozzi pizza"
     return 0;
 }
