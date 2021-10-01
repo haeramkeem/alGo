d5a46c10a0221179ee17e91d4899fbad841939cb
saltwalks <ewqdsacxz2345@gmail.com>
2021-10-02 02:27:08 +0900

    code cleanup

--- collections/heap/MinInt.go
+++ collections/heap/MinInt.go
@@ -10,47 +10,35 @@ func NewMinInt(size int) *MinInt {
 func (h *MinInt) Push(el int) error {
 	if h.Tail >= len(h.Tree) { return errors.New("heap: full") }
 	h.Tree[h.Tail] = el
+	if h.Tail > 0 { h.sortAfterPush(h.Tail) }
 	h.Tail++
-	if h.Tail < 2 {
-		h.sortAfterPush(h.Tail)
-	}
 	return nil
 }
 
 func (h *MinInt) Pop() (int, error) {
-	if h.Tail == 0 {
-		return 0, errors.New("heap: empty")
-	}
+	if h.Tail == 0 { return 0, errors.New("heap: empty") }
 	target := h.Tree[0]
 	h.Tree[0] = h.Tree[h.Tail - 1]
 	h.Tail--
-	h.sortAfterPop(1)
+	h.sortAfterPop(0)
 	return target, nil
 }
 
 /* ----- PRIVATE ----- */
 func (h *MinInt) sortAfterPush(cur int) {
-	if cur != 1 && h.Tree[cur - 1] < h.Tree[cur / 2 - 1] {
-		h.Tree[cur - 1], h.Tree[cur / 2 - 1] = h.Tree[cur / 2 - 1], h.Tree[cur - 1]
-		h.sortAfterPush(cur / 2)
+	if cur > 0 && h.Tree[cur] < h.Tree[(cur - 1) / 2] {
+		h.Tree[cur], h.Tree[(cur - 1) / 2] = h.Tree[(cur - 1) / 2], h.Tree[cur]
+		h.sortAfterPush((cur - 1) / 2)
 	}
 }
 
 func (h *MinInt) sortAfterPop(cur int) {
-	if cur * 2 - 1 >= h.Tail { return }
-	if cur * 2 == h.Tail {
-		if h.Tree[cur - 1] > h.Tree[cur * 2 - 1] {
-			h.Tree[cur - 1], h.Tree[cur * 2 - 1] = h.Tree[cur * 2 - 1], h.Tree[cur - 1]
-		}
-		return
-	}
-	if h.Tree[cur * 2 - 1] < h.Tree[cur - 1] && h.Tree[cur * 2 - 1] < h.Tree[cur * 2] {
-		h.Tree[cur * 2 - 1], h.Tree[cur - 1] = h.Tree[cur - 1], h.Tree[cur * 2 - 1]
-		h.sortAfterPop(cur * 2)
-		return
-	} else if h.Tree[cur * 2] < h.Tree[cur - 1] && h.Tree[cur * 2] < h.Tree[cur * 2 - 1] {
-		h.Tree[cur * 2], h.Tree[cur - 1] = h.Tree[cur - 1], h.Tree[cur * 2]
+	if cur * 2 + 1 < h.Tail && h.Tree[cur * 2 + 1] < h.Tree[cur] {
+		h.Tree[cur * 2 + 1], h.Tree[cur] = h.Tree[cur], h.Tree[cur * 2 + 1]
 		h.sortAfterPop(cur * 2 + 1)
-		return
+	}
+	if cur * 2 + 2 < h.Tail && h.Tree[cur * 2 + 2] < h.Tree[cur] {
+		h.Tree[cur * 2 + 2], h.Tree[cur] = h.Tree[cur], h.Tree[cur * 2 + 2]
+		h.sortAfterPop(cur * 2 + 2)
 	}
 }
